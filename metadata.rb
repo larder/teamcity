09fd2cfb7078fff1d80c4b395aa0a0e423674694
Paul Morton <pmorton@biaprotect.com>
2011-12-03 15:02:13 -0800

    Update License Information

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures teamcity"
+maintainer       "Paul Morton"
+maintainer_email "larder-project@gmail.com"
+license          "Apache License, Version 2.0"
+description      "Provides integration with teamcity for getting bits from teamcity"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
