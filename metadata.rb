19b6995b178b02bb3ad44796ab395d4d1ef04d14
Malte Swart <chef@malteswart.de>
2013-02-22 01:43:09 +0100

    update maintainer information

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'teamcity'
-maintainer       "Paul Morton"
-maintainer_email "larder-project@gmail.com"
+maintainer       "Malte Swart"
+maintainer_email "chef@maltswart.de"
 license          "Apache License, Version 2.0"
 description      "Provides integration with teamcity for getting bits from teamcity or setup build agents"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
