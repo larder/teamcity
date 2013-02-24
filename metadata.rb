f6c22482035c871d870aeac304a5f5eb81067230
Malte Swart <chef@malteswart.de>
2013-02-24 15:40:48 +0100

    release 0.2.1 - bug fix release (template error)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef@maltswart.de"
 license          "Apache License, Version 2.0"
 description      "Provides integration with teamcity for getting bits from teamcity or setup build agents"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 recommends "java"
 
